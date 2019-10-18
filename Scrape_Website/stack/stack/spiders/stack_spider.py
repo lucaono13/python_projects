f715237bfd2c270ef6617f33de64b09e13a05018
lucaono13 <gionorato@gmail.com>
2019-10-18 14:03:07 -0700

    Got all Forwards/Defenders/Goalies and their 2019 contract cost

--- Scrape_Website/stack/stack/spiders/stack_spider.py
+++ Scrape_Website/stack/stack/spiders/stack_spider.py
@@ -10,20 +10,56 @@ class StackSpider(Spider):
     ]
 
     def parse(self, response):
-        questions = Selector(response).xpath('//div[@class="cb team_c"]/table[@class="tbl fixed"]/tbody[1]/tr[@class="odd c"]')
+        forward1 = Selector(response).xpath('//div[@class="cb team_c"]/table[@class="tbl fixed"]/tbody[1]/tr[@class="odd c"]')
+        forward2 = Selector(response).xpath('//div[@class="cb team_c"]/table[@class="tbl fixed"]/tbody[1]/tr[@class="even c"]')
 
-        for question in questions:
+        for forward in forward1:
             item = StackItem()
-            item['name'] = question.xpath('td[@class="left"]/a/text()').extract()[0]
-            item['yrs_rem'] = question.xpath('td[@class="tm_yrr hide"]/text()').extract()[0]
-            item['cont_19'] = question.xpath('td[7]/span[@class="cap rel mtv"]/span[@class="num"]/text()').extract()[0]
+            item['name'] = forward.xpath('td[@class="left"]/a/text()').extract()[0]
+            item['yrs_rem'] = forward.xpath('td[@class="tm_yrr hide"]/text()').extract()[0]
+            item['cont_19'] = forward.xpath('td[7]/span[1]/span[@class="num"]/@data-num').extract()[0]
             yield item
 
-        questions2 = Selector(response).xpath('//div[@class="cb team_c"]/table[@class="tbl fixed"]/tbody[1]/tr[@class="even c"]')
+        #questions2 = Selector(response).xpath('//div[@class="cb team_c"]/table[@class="tbl fixed"]/tbody[1]/tr[@class="even c"]')
+        #questions2 = Selector(response).xpath('//div[@class="cb team_c"]/table[@class="tbl fixed"]/tbody[1]/tr[@class="even c"]')
 
-        for question in questions2:
+        for forward in forward2:
             item = StackItem()
-            item['name'] = question.xpath('td[@class="left"]/a/text()').extract()[0]
-            item['yrs_rem'] = question.xpath('td[@class="tm_yrr hide"]/text()').extract()[0]
-            item['cont_19'] = question.xpath('td[7]/span[@class="cap rel mtv"]/span[@class="num"]/text()').extract()[0]
+            item['name'] = forward.xpath('td[@class="left"]/a/text()').extract()[0]
+            item['yrs_rem'] = forward.xpath('td[@class="tm_yrr hide"]/text()').extract()[0]
+            item['cont_19'] = forward.xpath('td[7]/span[1]/span[@class="num"]/@data-num').extract()[0]
+            yield item
+
+        defense1 = Selector(response).xpath('//div[@class="cb team_c"]/table[@class="tbl fixed"]/tbody[2]/tr[@class="odd c"]')
+        defense2 = Selector(response).xpath('//div[@class="cb team_c"]/table[@class="tbl fixed"]/tbody[2]/tr[@class="even c"]')
+
+        for defd in defense1:
+            item = StackItem()
+            item['name'] = defd.xpath('td[@class="left"]/a/text()').extract()[0]
+            item['yrs_rem'] = defd.xpath('td[@class="left"]/a/text()').extract()[0]
+            item['cont_19'] = defd.xpath('td[7]/span[1]/span[@class="num"]/@data-num').extract()[0]
+            yield item
+
+        for defd in defense2:
+            item = StackItem()
+            item['name'] = defd.xpath('td[@class="left"]/a/text()').extract()[0]
+            item['yrs_rem'] = defd.xpath('td[@class="left"]/a/text()').extract()[0]
+            item['cont_19'] = defd.xpath('td[7]/span[1]/span[@class="num"]/@data-num').extract()[0]
+            yield item
+
+        goalie1 = Selector(response).xpath('//div[@class="cb team_c"]/table[@class="tbl fixed"]/tbody[3]/tr[@class="odd c"]')
+        goalie2 = Selector(response).xpath('//div[@class="cb team_c"]/table[@class="tbl fixed"]/tbody[3]/tr[@class="even c"]')
+
+        for goal in goalie1:
+            item = StackItem()
+            item['name'] = goal.xpath('td[@class="left"]/a/text()').extract()[0]
+            item['yrs_rem'] = goal.xpath('td[@class="left"]/a/text()').extract()[0]
+            item['cont_19'] = goal.xpath('td[7]/span[1]/span[@class="num"]/@data-num').extract()[0]
+            yield item
+
+        for goal in goalie2:
+            item = StackItem()
+            item['name'] = goal.xpath('td[@class="left"]/a/text()').extract()[0]
+            item['yrs_rem'] = goal.xpath('td[@class="left"]/a/text()').extract()[0]
+            item['cont_19'] = goal.xpath('td[7]/span[1]/span[@class="num"]/@data-num').extract()[0]
             yield item
