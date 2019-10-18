db2fdccf88d560a6cdb25f4028ed711c484e8938
lucaono13 <gionorato@gmail.com>
2019-10-17 20:06:34 -0700

    Started Web Scraping website other than StackOverflow

--- Scrape_Website/stack/stack/items.py
+++ Scrape_Website/stack/stack/items.py
@@ -6,9 +6,13 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.item import Item, Field
 
 
-class StackItem(scrapy.Item):
+class StackItem(Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
+    name = Field()
+    yrs_rem = Field()
+    cont_19 = Field()
     pass
